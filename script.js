69be1a712174b7e73238fce7c0d7b38ab879bf1e
Anderson Echavarria Blandon <AndersonEchavarria@comfama.com.co>
2023-06-01 23:09:43 -0500

    se realiza segundo ejercicio

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ while (opcion !== 0) {
   opcion = parseInt(prompt('MEMU DE EJERCICIOS: \n 1. Ordenar tres números ingresador por teclado de mayor a menor y de menor a mayor \n 2. Calcular el Area de una circunferencia \n 3. De acuerdo a las medidas de los lados de un triangulo, mostrar de qué tipo es este \n 4. Ingresar dos números por teclado y definir si uno es amigo del otro \n 5. Calcular el valor de una compra recibiendo un valor de compra y una cantidad \n 6. Recibir cuatro notas de un estudiante para determinar si gana o pierde \n 0. Salir '));
 
   if (opcion === 1){
+    contOpcion1++
     let num1 = Number(prompt('Ingresar numero: '));
 let num2 = Number(prompt('Ingresar numero: '));
 let num3 = Number(prompt('Ingresar numero: '));
@@ -34,7 +35,15 @@ if (num1 < num2 ) {
     console.log("Menor a mayor " + num2 +" "+ num1 +" "+ num3);
 }else{ if(num3 < num2){console.log ("Menor a mayor " + num3 +"  "+ num2 +"  "+ num1);}}
 }
-  }
+}else if (opcion === 2){
+    contOpcion2++
+    const pi = 3.1416
+    let radio = parseFloat(prompt('Ingrese el radio del circulo: '))
+    
+    console.log('El area del circulo es: '+ pi*radio*radio);
+
+}
+
 
 
 }
